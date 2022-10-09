9792b8b4bfbaf01f0fb597f114ad72fc6a4dfa89
Elan Nurjailani <elan.nrj@gmail.com>
2022-10-09 15:07:10 +0700

    feat(graphql): implement graphql connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv')
+const { graphqlHTTP } = require('express-graphql')
+const bodyParser = require('body-parser')
 
 const { connectDB } = require('./db')
+const schema = require('./graphql')
 
 const app = express()
 
@@ -12,6 +16,16 @@ connectDB()
 app.get('/', (_req, res) => {
   res.json({ message: 'Welcome 💡! Go to /graphql' })
 })
+
+app.use(
+  '/graphql',
+  cors(),
+  bodyParser.json(),
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+)
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on PORT ${process.env.PORT} 🚀`)
